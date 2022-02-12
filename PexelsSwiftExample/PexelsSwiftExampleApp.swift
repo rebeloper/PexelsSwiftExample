8a82f0c7116d33965aecc45099b9046646f31f4c
Alex Nagy <therebeloper@gmail.com>
2022-02-12 20:05:13 +0200

    Initial Commit

---
+++ PexelsSwiftExample/PexelsSwiftExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PexelsSwiftExampleApp.swift
+//  PexelsSwiftExample
+//
+//  Created by Alex Nagy on 12.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct PexelsSwiftExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
